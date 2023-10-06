e3fc46b8941a21d12ff6e0e152169595cabfcb38
CrazyBananaPie <oleksiy.sirobaba@gmail.com>
2023-10-06 11:16:59 +0200

    Made grand refactoring and small fixes

--- Algorithms-and-Data-Structures/PR5-SearchAlgorithms/search_algorithms_demo.py
+++ Algorithms-and-Data-Structures/PR5-SearchAlgorithms/search_algorithms_demo.py
@@ -1,41 +1,46 @@
 import random
-from pr_2 import RAM, Generator
-from find_alg import FindAlg
+
+from data_structures import OrganizedArray
+from models_and_generators import RAM, Generator
+from search_algorithms import SearchAlgorithms
 
 print("Standard preparing of class objects...")
-f = FindAlg()
-g = Generator()
+search_algorithms = SearchAlgorithms()
+organized_array = OrganizedArray()
+gen = Generator()
 
 print("Creating an organized array, filled with values..")
 for _ in range(6):
-    f.add(g.generate_single())
+    organized_array.add(gen.generate_single())
 example = RAM("Kingston", "ZP", 6, "DDR3", 1666, 3)
-f.add(example)
+organized_array.add(example)
 
 print("\nThe elements of array:")
-for key, el in enumerate(f.get_all()):
-    print(f"#{key}:  Element =--->  {el}")
+for key, element in enumerate(organized_array.get_all()):
+    print(f"#{key}:  Element =--->  {element}")
 
-print("""\nMETHOD OBJ_FIND
+array_data = organized_array.get_all()
+
+print("""\nMETHOD FIND_OBJECT
 Testing with:
 1 - variable from the list
 2 - variable not from the list""")
 
-example1 = f.get_all()[random.randint(0, 7)]
+example1 = array_data[random.randint(0, 7)]
 print(f"\n1. Trying to find the chosen element: {example1} ...")
-print(f"Result: {f.obj_find(example1)}")
+print(f"Result: {search_algorithms.find_object(array_data, example1)}")
 
 example2 = example1
-while example2 in f.get_all():
-    example2 = g.generate_single()
+while example2 in array_data:
+    example2 = gen.generate_single()
 print(f"\n2. Trying to find the failed element: {example2} ...")
-print(f"Result: {f.obj_find(example2)}")
+print(f"Result: {search_algorithms.find_object(array_data, example2)}")
 
 print("\nThe elements of array:")
-for key, el in enumerate(f.get_all()):
-    print(f"#{key}:  Element =--->  {el}")
+for key, element in enumerate(array_data):
+    print(f"#{key}:  Element =--->  {element}")
 
-print("""\nMETHOD FIND_BY_ATTR
+print("""\nMETHOD FIND_OBJECT_BY_ATTRIBUTES
 Testing with:
 1 - variable from the list
 2 - variable not from the list
@@ -48,21 +53,21 @@ element: {example}
 attributes: {attrs}
 values: {values}""")
 
-print(f"Result: {f.find_by_attr(attrs, values)}")
+print(f"Result: {search_algorithms.find_object_by_attributes(array_data, attrs, values)}")
 
 values = ['Kingstone', 'ZPRQ', 613]
 print(f"""\n2. Trying to find the failed element by attributes (changing the searched values of attributes)...
 attributes: {attrs}
 values: {values}""")
 
-print(f"Result: {f.find_by_attr(attrs, values)}")
+print(f"Result: {search_algorithms.find_object_by_attributes(array_data, attrs, values)}")
 
 attrs = ['manufacturer', 'season', 'size']
 values = ['Kingston', 'ZP', 6]
-example2 = g.generate_single()
+example2 = gen.generate_single()
 print(f"""\n3. Trying to find the element by error of the attributes (checking the AttributeError)...
 element: {example2}
 attributes: {attrs}
 values: {values}""")
 
-print(f"Result: {f.find_by_attr(attrs, values)}")
+print(f"Result: {search_algorithms.find_object_by_attributes(array_data, attrs, values)}")
